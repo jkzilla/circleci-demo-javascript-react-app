b58d31370e3c84c6146744b26fecaa2609a2f7e9
johanna <johanna@circleci.com>
2022-11-22 22:03:58 -0800

    test

--- bashScript.sh
+++ bashScript.sh
@@ -7,14 +7,12 @@ while read -r line; do
     https://circleci.com/graphql-unstable \
     -d '{"operationName":"Context","variables":{"contextId": "'"$line"'"},"query":"query Context($contextId: ID!) {\n  context(id: $contextId) {\n    id\n    resources {\n      createdAt\n      truncatedValue\n      variable\n      __typename\n    }\n    groups {\n      edges {\n        node {\n          id\n          name\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    owner {\n      ... on Organization {\n        id\n        groups {\n          edges {\n            node {\n              id\n              name\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    name\n    __typename\n  }\n}\n"}' | jq -r '.data.context.resources[].truncatedValue')
     echo $TRUNCATED_VALUE
-    if [ "$MATCHED_VALUE" = *"$TRUNCATED_VALUE"* ]; then
+    if [[ "$MATCHED_VALUE" = *"$TRUNCATED_VALUE"* ]]; then
         echo "Matched"
         export CONTEXT_ID=$line
         echo $CONTEXT_ID
-
         export VARIABLE_NAME=$(curl -X POST -H content-type:application/json -H Authorization:$CIRCLE_TOKEN \ 'https://circleci.com/graphql-unstable' \ -d '{"operationName":"Context","variables":{"contextId": "'"$line"'"},"query":"query Context($contextId: ID!) {\n  context(id: $contextId) {\n    id\n    resources {\n      createdAt\n      truncatedValue\n      variable\n      __typename\n    }\n    groups {\n      edges {\n        node {\n          id\n          name\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    owner {\n      ... on Organization {\n        id\n        groups {\n          edges {\n            node {\n              id\n              name\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    name\n    __typename\n  }\n}\n"}' | jq -r '.data.context.resources[].variable')
         echo $VARIABLE_NAME
-        break
     fi
 done < listIds.txt
 
