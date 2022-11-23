c24ff2c7dd64931d5e005365a898e2b7c55677cf
johanna <johanna@circleci.com>
2022-11-22 21:56:29 -0800

    Test

--- bashScript.sh
+++ bashScript.sh
@@ -6,13 +6,17 @@ while read -r line; do
     export TRUNCATED_VALUE=$(curl -X POST -H content-type:application/json -H Authorization:$CIRCLE_TOKEN \
     https://circleci.com/graphql-unstable \
     -d '{"operationName":"Context","variables":{"contextId": "'"$line"'"},"query":"query Context($contextId: ID!) {\n  context(id: $contextId) {\n    id\n    resources {\n      createdAt\n      truncatedValue\n      variable\n      __typename\n    }\n    groups {\n      edges {\n        node {\n          id\n          name\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    owner {\n      ... on Organization {\n        id\n        groups {\n          edges {\n            node {\n              id\n              name\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    name\n    __typename\n  }\n}\n"}' | jq -r '.data.context.resources[].truncatedValue')
-    
+    echo $TRUNCATED_VALUE
+    if [ "$TRUNCATED_VALUE" = "$MATCHED_VALUE" ]; then
+        echo "Matched"
+        export CONTEXT_ID=$line
+        echo $CONTEXT_ID
+        break
+    fi
     export VARIABLE_NAME=$(curl -X POST -H content-type:application/json -H Authorization:$CIRCLE_TOKEN \
     https://circleci.com/graphql-unstable \
     -d '{"operationName":"Context","variables":{"contextId": "'"$line"'"},"query":"query Context($contextId: ID!) {\n  context(id: $contextId) {\n    id\n    resources {\n      createdAt\n      truncatedValue\n      variable\n      __typename\n    }\n    groups {\n      edges {\n        node {\n          id\n          name\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    owner {\n      ... on Organization {\n        id\n        groups {\n          edges {\n            node {\n              id\n              name\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    name\n    __typename\n  }\n}\n"}' | jq -r '.data.context.resources[].variable')
-    
-    if [[ "$MATCHED_VALUE" == *"$TRUNCATED_VALUE"* ]]; then
-    echo "WE MATCH!!!!!!"
-    echo $VARIABLE_NAME
-    fi
+   echo $VARIABLE_NAME
 done < listIds.txt
+
+
